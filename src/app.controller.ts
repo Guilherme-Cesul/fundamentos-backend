1830c5632219e5b1e498c2d518c14bcbbbc326c5
Guilherme Sartori <guilherme.2023014204@alunocesul.com>
2025-04-15 21:22:55 -0300

    criando Post, GetAll, GetById, Put, Patch e Delete

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,24 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post, HttpCode, Get, Delete, Param, Put, Patch } from '@nestjs/common';
 import { z } from 'zod';
 import { ZodValidationPipe } from './pipes/zod-validation-pipe';
+
+function isValidCPF(cpf: string): boolean {
+  cpf = cpf.replace(/[^\d]+/g, '');
+
+  if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+
+  let sum = 0;
+  for (let i = 0; i < 9; i++) sum += +cpf.charAt(i) * (10 - i);
+  let rev = 11 - (sum % 11);
+  if (rev === 10 || rev === 11) rev = 0;
+  if (rev !== +cpf.charAt(9)) return false;
+
+  sum = 0;
+  for (let i = 0; i < 10; i++) sum += +cpf.charAt(i) * (11 - i);
+  rev = 11 - (sum % 11);
+  if (rev === 10 || rev === 11) rev = 0;
+  return rev === +cpf.charAt(10);
+}
 
 const createProductBodySchema = z.object({
   name: z.string().min(5).max(30),
@@ -9,19 +27,78 @@ const createProductBodySchema = z.object({
   year: z.number().gt(4),
   brand: z.string().min(3).max(15),
   email: z.string().email(),
-  cpf: z.string().regex(/^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/),
+  cpf: z.string().regex(/^\d{11}$/, {
+    message: "CPF deve conter exatamente 11 digitos numéricos",
+  })
+  .refine(isValidCPF, {
+    message: "CPF Invalid",
+  })
 })
 
 const bodyValidationPipe = new ZodValidationPipe(createProductBodySchema);
 
 type CreateProductBodySchema = z.infer<typeof createProductBodySchema>;
 
+const updateProductBodySchema = z.object({
+  name: z.string().min(5).max(30),
+  model: z.string().min(5).max(30),
+  dateManufacture: z.string().date(),
+  year: z.number().gt(4),
+  brand: z.string().min(3).max(15),
+  email: z.string().email(),
+  cpf: z.string().regex(/^\d{11}$/, {
+    message: "CPF deve conter exatamente 11 digitos numéricos",
+  })
+  .refine(isValidCPF, {
+    message: "CPF Invalid",
+  })
+})
+
+const updateBodyValidationPipe = new ZodValidationPipe(updateProductBodySchema);
+
+type UpdateProductBodySchema = z.infer<typeof updateProductBodySchema>;
+
+
 @Controller("/products")
 export class AppController {
   constructor() {}
 
   @Post()
-  create(@Body(bodyValidationPipe) body: CreateProductBodySchema): string {
-    return "create";
+  @HttpCode(201)
+  create(@Body(bodyValidationPipe) body: CreateProductBodySchema) {
+   const { brand, cpf, dateManufacture, email, model, name, year } = body;
+
+  };
+
+  @Get()
+  @HttpCode(200)
+  findAll(): string {
+    return "Produtos"
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  findById(@Param('id') id: string) {
+
+  }
+  
+  @Put()
+  @HttpCode(204)
+  update(@Param('id') id: string, @Body(updateBodyValidationPipe) body: UpdateProductBodySchema) {
+    
+  }
+
+  @Patch('/:id/status')
+  @HttpCode(204)
+  updateStatus(@Param('id') id: string, @Body(updateBodyValidationPipe) body: UpdateProductBodySchema) {
+
+  }
+
+  @Delete(':id')
+  @HttpCode(200)
+  remove(@Param('id') id: string) {
+
   }
 }
+
+
