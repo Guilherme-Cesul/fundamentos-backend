bd3aead697956b658a2f2332d1ff938b9178e796
Guilherme Sartori <guilherme.2023014204@alunocesul.com>
2025-04-29 19:53:17 -0300

    feat: desenvolvendo create-product.service.ts

---
+++ src/create-product.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@nestjs/common";
+
+interface CreateProductServiceRequest {
+  name: string;
+  model: string;
+  dateManufacture: string;
+  year: string;
+  brand: string;
+  email: string;
+  cpf: string;
+}
+
+type CreateProductServiceResponse = {
+  product: CreateProductService;
+};
+
+@Injectable()
+export class CreateProductService {
+  constructor() { }
+
+  async execute({
+    name,
+    model,
+    dateManufacture,
+    year,
+    brand,
+    email,
+    cpf
+
+  }: CreateProductServiceRequest): Promise<CreateProductServiceResponse> {
+    return null;
+  }
+}
